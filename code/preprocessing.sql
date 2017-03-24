65c4ea26d4508fe6dc92b2d08132cd0d847ffde5
Hippolyte Jacomet <hippolytej@users.noreply.github.com>
2017-03-24 19:04:42 +0100

    Update and rename query.sql to preprocessing.sql

--- code/preprocessing.sql
+++ code/preprocessing.sql
@@ -4,17 +4,15 @@ CREATE TABLE contacts_processed (
   id INT UNSIGNED NOT NULL,
   zip_code    VARCHAR(5),
   code_geo VARCHAR(5),
-  town_clean  VARCHAR(60),
-  nb_contacts	INT,
+  town_clean  VARCHAR(60)
   PRIMARY KEY (id),
   KEY twn (town_clean)
   )
 ENGINE = MyISAM;
 
 INSERT INTO contacts_processed
-SELECT id, zip_code, code_geo, town_clean, count(*) as nb_contacts
-FROM contacts
-GROUP BY code_geo;
+SELECT id, zip_code, code_geo, town_clean
+FROM contacts;
 
 UPDATE contacts_processed
 SET code_geo = '13055' 
