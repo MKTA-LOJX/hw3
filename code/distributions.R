ef6615a7885eb82bc873ff9feae3bde8c2f9bdd6
hippolytej <hippolyte.jacomet@gmail.com>
2017-03-27 00:07:52 +0200

    hakunamatata

--- code/distributions.R
+++ code/distributions.R
@@ -17,10 +17,10 @@ db = odbcConnect("mysql_server_64", uid="root") # change if needed with name of 
 # Distribution
 
 # Get the data
-query3 ="SELECT id, zip_code, code_geo, town_clean, count(*) as nb_contacts
+query ="SELECT id, zip_code, code_geo, town_clean, count(*) as nb_contacts
 FROM charity2.contacts_processed
 GROUP BY code_geo"
-donators = sqlQuery(db,query3)
+donators = sqlQuery(db,query)
 head(donators)
 
 # Compute percentage by code_geo and set to text format
@@ -36,11 +36,10 @@ head(inscrits)
 
 # Merge the two
 dist = merge(x = donators, y = inscrits, by = "code_geo", all.x = TRUE)
-dist[,'relat_dist'] = dist$perc_contacts/dist$perc_pop
+dist[,'relat_dist'] = dist$perc_contacts/dist$perc_pop - 1
 head(dist)
 
 # Mismatches... TODO
-dist[is.na(dist$nb_inscrits),]
 nrow(dist[is.na(dist$nb_inscrits),])
 
 # Ignore DOM TOM for the moment
@@ -48,8 +47,44 @@ dist=dist[complete.cases(dist),]
 
 # Convert code_geo to numeric for ggplot
 gr2 = ggplot(data= dist, aes(x=code_geo, y=relat_dist))+
-    geom_histogram(alpha=.7, stat="sum")+
+    geom_bar(alpha=.7, stat="sum")+
     theme(axis.text.x = element_blank())+
+    #scale_x_discrete(breaks=waiver())+
     labs(title = "Distribution relative des contacts par code insee",y= "Distribution relative", x= "Code Insee")
 print(gr2)
+
+# By departement ----------------------------------------
+dist['departement'] = substring(dist$code_geo,1,2)
+head(dist)
+aggregate(dist$perc_contacts, dist$perc_pop, by=list(Category=dist$departement), FUN=sum)
+
+charity_dist = aggregate(perc_contacts ~ departement, dist, sum)
+france_dist = aggregate(perc_pop ~ departement, dist, sum)
+
+
+gr3 = ggplot(data= charity_dist, aes(x=departement, y=perc_contacts))+
+    geom_bar(alpha=.7, stat="sum")+
+    theme(axis.text.x = element_blank())+
+    scale_x_discrete(breaks=1:5)+
+    labs(title = "Distribution relative des contacts par code insee",y= "Distribution contacts", x= "Code Insee")
+print(gr3)
+
+gr4 = ggplot(data=france_dist, aes(x=departement, y=perc_pop))+
+    geom_bar(alpha=.7, stat="sum")+
+    theme(axis.text.x = element_blank())+
+    scale_x_discrete(breaks=1:5)+
+    labs(title = "Distribution relative des contacts par département",y= "Distribution votants", x= "Code Insee")
+print(gr4)
+
+# Relative distribution
+relat_dist = charity_dist
+colnames(relat_dist) = c('departement', 'ratio')
+relat_dist$ratio = charity_dist$perc_contacts / france_dist$perc_pop - 1
+
+gr5 = ggplot(data=relat_dist, aes(x=departement, y=ratio))+
+    geom_bar(alpha=.7, stat="sum")+
+    theme(axis.text.x = element_blank())+
+    scale_x_discrete(breaks=1:5)+
+    labs(title = "Distribution relative des contacts par département",y= "Distribution relative", x= "Code Insee")
+print(gr5)
 
